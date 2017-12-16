e1e12f1edbe299b3b8701ea973e577b3f957b2b0
Misty <ceileen345@gmail.com>
2017-12-16 15:46:13 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,83 @@
+$(document).ready(function() {
+    var berryEffects = {
+        "1": "Cures paralysis.",
+        "2": "Cures sleep.",
+        "3": "Cures poison.",
+        "4": "Cures burn.",
+        "5": "Cures freeze.",
+        "6": "Restores 10 PP.",
+        "7": "Restores 10 HP.",
+        "8": "Cures confusion.",
+        "9": "Cures any status condition and confusion.",
+        "10": "Restores 30 HP (Gen III) or 25% HP (Gen IV+).",
+        "11": "When HP falls below 25%, restores 50% HP, but confuses Pokémon that dislike spicy food (12.5% when below 50% before Gen. VII).",
+        "12": "When HP falls below 25%, restores 50% HP, but confuses Pokémon that dislike dry food (12.5% when below 50% before Gen. VII).",
+        "13": "When HP falls below 25%, restores 50% HP, but confuses Pokémon that dislike sweet food (12.5% when below 50% before Gen. VII).",
+        "14": "When HP falls below 25%, restores 50% HP, but confuses Pokémon that dislike bitter food (12.5% when below 50% before Gen. VII).",
+        "15": "When HP falls below 25%, restores 50% HP, but confuses Pokémon that dislike sour food (12.5% when below 50% before Gen. VII).",
+        "16": "None.",
+        "17": "None.",
+        "18": "None.",
+        "19": "None.",
+        "20": "None.",
+        "21": "Lowers HP EVs, raises friendship.",
+        "22": "Lowers Attack EVs, raises friendship.",
+        "23": "Lowers Defense EVs, raises friendship.",
+        "24": "Lowers Special Attack EVs, raises friendship.",
+        "25": "Lowers Special Defense EVs, raises friendship.",
+        "26": "Lowers Speed EVs, raises friendship.",
+        "27": "None.",
+        "28": "None.",
+        "29": "None.",
+        "30": "None.",
+        "31": "None.",
+        "32": "None.",
+        "33": "None.",
+        "34": "None.",
+        "35": "None.",
+        "36": "Decreases damage taken by the holder from a supereffective Fire-type move.",
+        "37": "Decreases damage taken by the holder from a supereffective Water-type move.",
+        "38": "Decreases damage taken by the holder from a supereffective Electric-type move.",
+        "39": "Decreases damage taken by the holder from a supereffective Grass-type move.",
+        "40": "Decreases damage taken by the holder from a supereffective Ice-type move.",
+        "41": "Decreases damage taken by the holder from a supereffective Fighting-type move.",
+        "42": "Decreases damage taken by the holder from a supereffective Poison-type move.",
+        "43": "Decreases damage taken by the holder from a supereffective Ground-type move.",
+        "44": "Decreases damage taken by the holder from a supereffective Flying-type move.",
+        "45": "Decreases damage taken by the holder from a supereffective Psychic-type move.",
+        "46": "Decreases damage taken by the holder from a supereffective Bug-type move.",
+        "47": "Decreases damage taken by the holder from a supereffective Rock-type move.",
+        "48": "Decreases damage taken by the holder from a supereffective Ghost-type move.",
+        "49": "Decreases damage taken by the holder from a supereffective Dragon-type move.",
+        "50": "Decreases damage taken by the holder from a supereffective Dark-type move.",
+        "51": "Decreases damage taken by the holder from a supereffective Steel-type move.",
+        "52": "Decreases damage taken by the holder from a Normal-type move.",
+        "53": "Raises Attack when HP falls below 25%.",
+        "54": "Raises Defense when HP falls below 25%.",
+        "55": "Raises Speed when HP falls below 25%.",
+        "56": "Raises Special Attack when HP falls below 25%.",
+        "57": "Raises Special Defense when HP falls below 25%.",
+        "58": "Raises critical hit ratio when HP falls below 25%.",
+        "59": "Sharply raises a random stat when HP falls below 25%.",
+        "60": "Restores holder's HP if hit by a supereffective move.",
+        "61": "Raises the accuracy of one move when HP falls below 25%.",
+        "62": "Causes the holder to go first once when HP falls below 25%.",
+        "63": "Damages a Pokémon that hits the holder with a physical move.",
+        "64": "Damages a Pokémon that hits the holder with a special move.",
+        "65": "Decreases damage taken by the holder from a supereffective Fairy-type move.",
+        "66": "Raises Defense if holder is hit by a physical move.",
+        "67": "Raises Special Defense if holder is hit by a special move."
+    };
+    
+    $( "#usage" ).click(function() {
+        //stuff here
+    });
+    
+    $( "#flavor" ).click(function() {
+        //stuff here
+    });
+    
+    $( "#naturalgift" ).click(function() {
+        //stuff here
+    });
+});
